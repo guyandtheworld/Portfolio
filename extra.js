a4f162138c0040528856d75a0f71296f18985fdf
Adarsh S <4d4r5h.5@gmail.com>
2017-04-08 23:38:44 +0530

    Add layers to portfolio

---
+++ extra.js
@@ -0,0 +1,10 @@
+function layer2() {
+    document.getElementById('layer-two').style.display='block';
+    document.getElementById('layer-two').scrollIntoView();
+}
+
+function layer3() {
+    document.getElementById('layer-three').style.display='block';
+    document.getElementById('layer-three').scrollIntoView();
+
+}
